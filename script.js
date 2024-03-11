efaf820d165eaff97e0e11488b12e58f11432793
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-03-11 16:51:11 +0000

    feat: add Book object and function to add books to library with test data

---
+++ script.js
@@ -0,0 +1,29 @@
+const myLibrary = [] // All of your book objects are going to be stored in an array
+
+// Object constructor
+function Book(author, title, pages, read) {
+    // details for the new book: author, title, number of pages, whether it’s been read and anything else you might want
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBook(book) {
+    // add a function to the script (not the constructor) that can take user’s input and store the new book objects into an array
+    myLibrary.push(book);
+}
+
+function displayLibrary() {
+    // Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”
+}
+
+testBook1 = new Book('Phillip Pullman', 'Northern Lights', 448, true);
+testBook2 = new Book('Phillip Pullman', 'The Subtle Knife', 368, true);
+testBook3 = new Book('Phillip Pullman', 'The Amber Spyglass', 544, false);
+
+addBook(testBook1);
+addBook(testBook2);
+addBook(testBook3);
+
+console.log(myLibrary);
