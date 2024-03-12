4005484d25b97eed8ae6fac83c5a5f28195c3186
naomi-tan <69682497+naomi-tan@users.noreply.github.com>
2024-03-12 00:06:45 +0000

    feat: add title and author to books, layout books

--- script.js
+++ script.js
@@ -15,20 +15,45 @@ function addBook(book) {
     console.log(`${book.title} was added to the library`);
 }
 
+function clearLibrary() {
+    booksNodes = document.querySelectorAll('.book');
+    for(let i = 0; i < booksNodes.length; ++i) {
+        library.removeChild(booksNodes[i]);
+    }
+}
+
 function displayLibrary() {
     // Write a function that loops through the array and displays each book on the page. You can display them in some sort of table, or each on their own “card”
     // get .library div
-    let library = document.querySelector('.library');
+    clearLibrary();
     for (let book of myLibrary) {
-        book = document.createElement('div');
-        book.classList.add('book');
-        library.appendChild(book);
+        bookNode = document.createElement('div');
+        bookNode.classList.add('book');
+        displayTitle = document.createElement('p');
+        displayTitle.textContent = book.title;
+        displayAuthor = document.createElement('p');
+        displayAuthor.textContent = book.author;
+        bookNode.appendChild(displayAuthor);
+        bookNode.appendChild(displayTitle);
+        library.appendChild(bookNode);
         console.log(book);
     }
 }
 
 function openModal() {
     modal.showModal();
+}
+
+function okPressed(event) {
+    event.preventDefault();
+    newTitle = document.querySelector('#title').value;
+    newAuthor = document.querySelector('#author').value;
+    newPages = document.querySelector('#pages').value;
+    newRead = document.querySelector('#read').value;
+    newBook = new Book(newAuthor, newTitle, newPages, newRead);
+    addBook(newBook);
+    displayLibrary();
+    closeModal();
 }
 
 function closeModal() {
@@ -47,6 +72,8 @@ addBook(testBook3);
 
 // MAIN CODE
 
+let library = document.querySelector('.library');
+
 displayLibrary();
 
 modal = document.querySelector('dialog');
@@ -55,7 +82,7 @@ newBookButton = document.querySelector('.new-book');
 newBookButton.addEventListener('click', openModal);
 
 okModalButton = document.querySelector('.ok-button');
-okModalButton.addEventListener('click', closeModal)
+okModalButton.addEventListener('click', okPressed);
 
 closeModalButton = document.querySelector('.cancel-button');
-closeModalButton.addEventListener('click', closeModal)
+closeModalButton.addEventListener('click', closeModal);
